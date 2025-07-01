006928e1ee18d1c695cf7738db250c45d44a8b76
Karlo-Markovic <karlo.markovic@ibm.com>
2025-07-01 15:23:01 +0200

    add react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BudgetsPage from "./pages/BudgetsPage";
+import OverviewPage from "./pages/OverviewPage";
+import TransactionsPage from "./pages/TransactionsPage";
+import PotsPage from "./pages/PotsPage";
+import RecurringBillsPage from "./pages/RecurringBillsPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/transactions" element={<TransactionsPage />} />
+        <Route path="/budgets" element={<BudgetsPage />} />
+        <Route path="/pots" element={<PotsPage />} />
+        <Route path="/recuring-bills" element={<RecurringBillsPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/RecurringBillsPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecurringBillsPage: React.FC = () => {
+  return <h1>Recurring bills</h1>;
+};
+
+export default RecurringBillsPage;
