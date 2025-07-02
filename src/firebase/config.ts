b6b161e254afa915de65edda08e095031f7163d6
IvanaPusic <pusicka97@gmail.com>
2025-07-02 08:09:34 +0200

    create config for firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,7 @@
 
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 interface Configuration {
     apiKey: string;
@@ -23,3 +25,7 @@ const firebaseConfig: Configuration = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db }
