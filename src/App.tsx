f210126520fde79619d40a1422dd73ec7fd73677
Karlo-Markovic <karlo.markovic@ibm.com>
2025-07-03 12:04:01 +0200

    add login UI

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,25 @@ import PotsPage from "./pages/PotsPage";
 import RecurringBillsPage from "./pages/RecurringBillsPage";
 import LoginPage from "./pages/LoginPage";
 import PrivateRoute from "./components/PrivateRoute";
+import PageWrapper from "./components/PageWrapper";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route element={<PrivateRoute />}>
-          <Route path="/" element={<OverviewPage />} />
-          <Route path="/transactions" element={<TransactionsPage />} />
-          <Route path="/budgets" element={<BudgetsPage />} />
-          <Route path="/pots" element={<PotsPage />} />
-          <Route path="/recuring-bills" element={<RecurringBillsPage />} />
-        </Route>
-      </Routes>
+      <PageWrapper>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<OverviewPage />} />
+            <Route path="/transactions" element={<TransactionsPage />} />
+            <Route path="/budgets" element={<BudgetsPage />} />
+            <Route path="/pots" element={<PotsPage />} />
+            <Route path="/recuring-bills" element={<RecurringBillsPage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </PageWrapper>
     </Router>
   );
 }
