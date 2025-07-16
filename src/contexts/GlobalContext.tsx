29209f8a040117779bf8f16b18af4f30ca4dc765
Karlo-Markovic <karlo.markovic@ibm.com>
2025-07-16 13:39:02 +0200

    add see all button functionality in BudgetCard

--- src/contexts/GlobalContext.tsx
+++ src/contexts/GlobalContext.tsx
@@ -87,22 +87,22 @@ export const GlobalProvider: React.FC<{ children: React.ReactNode }> = ({
         console.log(financialData);
       }
     });
-    return () => unsubscribe(); // Clean up on unmount
+    return () => unsubscribe();
   }, [currentUid, window.location.pathname]);
 
   useEffect(() => {
-    if (budgets.length === 0 || transactions.length === 0) return;
+    if (budgets.length === 0 || allTransactions.length === 0) return;
 
     const grouped: Record<string, Transaction[]> = {};
 
     budgets.forEach((budget) => {
-      grouped[budget.category] = transactions.filter(
+      grouped[budget.category] = allTransactions.filter(
         (transaction) => transaction.category === budget.category
       );
     });
     console.log("grouped", grouped);
     setTransactionsByCategory(grouped);
-  }, [budgets, transactions]);
+  }, [budgets, allTransactions]);
 
   const handleInput = (
     event: React.ChangeEvent & { target: HTMLInputElement }
@@ -127,6 +127,7 @@ export const GlobalProvider: React.FC<{ children: React.ReactNode }> = ({
     event: React.ChangeEvent & { target: HTMLSelectElement }
   ) => {
     const selectedCategory = event.target.value;
+
     setCategorySelect(selectedCategory);
 
     if (selectedCategory) {
@@ -179,6 +180,7 @@ export const GlobalProvider: React.FC<{ children: React.ReactNode }> = ({
     event: React.ChangeEvent & { target: HTMLSelectElement }
   ) => {
     const selectedCategory = event.target.value;
+
     setCategorySelect(selectedCategory);
     if (selectedCategory) {
       checkSelectedCategory(selectedCategory as Category);
